01956b8e32ed4b859f8b662a8b6cfaaf6d7659cb
ahmedsanad-uni <ahmedsanad1234@hotmail.com>
2025-11-11 09:12:58 +0200

    Update main.cpp
    Completed student and person class implementations

--- main.cpp
+++ main.cpp
@@ -10,19 +10,25 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
+Person ( string name, int id ) {
+this-> name = name;
+this -> id = id;
+}
 
-
-
-
-
-
+void display(){
+cout<<"Person's name is: " << this-> name << endl << "Person's id is: " << this-> id << endl;
+}
 
 // ==================== Student Class Implementation ====================
 
+Student ( int yearLevel, string major ) {
+this-> yearLevel = yearLevel;
+this-> major = major;
+}
 
-
-
-
+void display() {
+cout << "This student's year level is: " << this-> yearLevel << endl << "This student's major is: " << this-> major << endl;
+}
 
 // ==================== Instructor Class Implementation ====================
 
